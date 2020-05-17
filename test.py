7ea5468cd935970b5dededcc86f36d8d70417fd2
ivan-rakos <ivan.jimenezv@e-campus.uab.cat>
2020-05-18 00:36:24 +0200

    Creacion de fichero de tests

---
+++ test.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+    def test_algo(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
