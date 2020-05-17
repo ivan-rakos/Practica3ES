8d4f1ed9c4d00ededb12e199ba5ac57df8f1c4d6
David-Duran <59368742+David-Duran@users.noreply.github.com>
2020-05-18 00:29:34 +0200

    Update Cars.py

--- Cars.py
+++ Cars.py
@@ -1,4 +1,7 @@
 class Cars:
 
-    def __init__(self):
-        pass
+    def __init__(self, matricula, modelo, asientos):
+        self.matricula = matricula
+        self.modelo = modelo
+        self.asientos = asientos
+        self.disponible = True
