516249f07a4a82977c3fa46f35232faf4cba4c30
Xavier Lopez <xavier.lopezlar@e-campus.uab.cat>
2020-05-18 00:14:50 +0200

    Clase Hoteles y Clase Flights Creadas

--- Flights.py
+++ Flights.py
@@ -1,4 +1,7 @@
 class Flights:
 
-    def __init__(self):
-        pass
+    def __init__(self,codi_vol,destinacio,numero_passatgers):
+        self.codi_vol = codi_vol
+        self.destinacio = destinacio
+        self.numero_passatgers = numero_passatgers
+    pass
--- Hotels.py
+++ Hotels.py
@@ -1,4 +1,12 @@
 class Hotels:
 
-    def __init__(self):
+    def __init__(self, codi, nom, numero_hostes, numero_habitacio, durada_reserva):
+        self.codi = codi
+        self.nom = nom
+        self.numero_hostes = numero_hostes
+        self.numero_habitacio = numero_habitacio
+        self.durada_reserva = durada_reserva
+
+    def confirm_reserve(self,user,lista_hoteles):
+        #CODIGO
         pass
