43a7e1f61fbd723387f9b28cf9c7d35d61101873
ivan-rakos <ivan.jimenezv@e-campus.uab.cat>
2020-05-18 00:11:05 +0200

    Definicion basica de las clases Bank,PaymentData y User

--- Bank.py
+++ Bank.py
@@ -4,7 +4,9 @@ from . import PaymentData
 
 class Bank:
 
-    def __init__(self):
+    def __init__(self,usuario,datos_pago):
+        self.user=usuario
+        self.pago=datos_pago
         pass
 
     def do_payment(self, user: User, payment_data: PaymentData):
--- PaymentData.py
+++ PaymentData.py
@@ -1,4 +1,9 @@
 class PaymentData:
 
-    def __init__(self):
+    def __init__(self,tipo,titular,num,cod,imp):
+        self.tipo_pago = tipo
+        self.titular = titular
+        self.num_tarjeta = num
+        self.cod_seguridad = cod
+        self.importe = imp
         pass
