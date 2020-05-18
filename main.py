a539661c5850af937e45578c6238c5f54306ee50
ivan-rakos <ivan.jimenezv@e-campus.uab.cat>
2020-05-18 12:49:19 +0200

    Creacion main del programa principal

---
+++ main.py
@@ -0,0 +1,14 @@
+import User
+import Bank
+import Flights
+import PaymentData
+
+
+
+
+
+if __name__ == '__main__':
+    print("****AIR HOPPING*****")
+    print("Introduce el numero de viajeros")
+    viajeros = input()
+    print("Introduce un destino")
