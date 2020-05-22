d466d584285d6f42eb234a0d7c2a64243c76e7a3
David_Duran <skkiper2000@gmail.com>
2020-05-22 18:24:11 +0200

    first version v3

--- Travel.py
+++ Travel.py
@@ -1,16 +1,22 @@
 import User
 import Flights as f
 import Bank as b
+import Cars as c
+import Hotels as h
 import Skyscanner as s
-import PaymentData as p
+import Rentalcars as r
+import Booking as b
 
 class Travel:
-    def __init__(self,vuelo=[],dest=[],viajeros=1):
+
+    def __init__(self, vuelo=[], dest=[], viajeros=1):
         self.vuelos = vuelo
         self.destinos = dest
-        self.viajeros=viajeros
-        self.precio=0
-        self.pagado=False
+        self.viajeros = viajeros
+        self.cars = []
+        self.hoteles = []
+        self.precio = 0
+        self.pagado = False
         self.reservas = False
         self.updatePrecio()
 
@@ -45,8 +51,9 @@ class Travel:
 
     def updatePrecio(self):
         precio = 0
-        for i in self.vuelos:
-            precio += (i.precio * self.viajeros)
+        for i in self.vuelos: precio += (i.precio * self.viajeros)
+        for car in self.cars: precio += car.dias * car.precio_dia
+        for hotel in self.hoteles: precio += (hotel.precio_dia * hotel.numero_hostes * hotel.durada_reserva)
         self.precio=precio
 
     def payTravel(self,bank):
@@ -77,6 +84,39 @@ class Travel:
             if i not in destinosPosibles:
                 print("Ha habido algun error al confirmar el vuelo")
                 break
+
     def getVuelos(self):
         for i in self.vuelos:
             print("Vuelo con codigo: "+str(i.codigo)+", con destino: "+i.destino+", con "+str(i.num_viajeros)+" viajeros")
+
+    def addCar(self, car):
+        self.cars.append(car)
+        self.updatePrecio()
+
+    def delCar(self, car):
+        self.cars.remove(car)
+        self.updatePrecio()
+
+    def addHotel(self, hotel):
+        self.hoteles.append(hotel)
+        self.updatePrecio()
+
+    def delHotel(self, hotel):
+        self.hoteles.remove(hotel)
+        self.updatePrecio()
+
+    def confirmCars(self, user):
+        rent = r.Rentalcars()
+        if len(self.cars) > 0:
+            print("Vehiculos reservados correctamente") if rent.confirm_reserve(user, self.cars) else print(
+                "Error en la reserva de vehiculos")
+        else:
+            print("No hay vehiculos a reservar")
+
+    def confirmHotels(self, user):
+        book = b.Booking()
+        if len(self.hoteles) > 0:
+            print("Alojamientos reservados correctamente") if book.confirm_reserve(user, self.cars) else print(
+                "Error en la reserva de alojamientos")
+        else:
+            print("No hay alojamientos a reservar")
