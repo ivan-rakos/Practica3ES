f60108b530231c5259a783a157c317a86392d6d0
ivan-rakos <57662193+ivan-rakos@users.noreply.github.com>
2020-05-23 18:18:07 +0200

    Update User.py

--- User.py
+++ User.py
@@ -1,10 +1,10 @@
 class User:
 
-    def __init__(self,i,n,a,t,s,na):
-        self.id=i
-        self.nombre=n
-        self.apellidos=a
-        self.telefono=t
-        self.sexo=s
-        self.nacionalidad=na
-        pass
+    def __init__(self,id,nombre,apellidos,telefono,sexo,nacionalidad):
+        self.id=id
+        self.nombre=nombre
+        self.apellidos=apellidos
+        self.telefono=telefono
+        self.sexo=sexo
+        self.nacionalidad=nacionalidad
+        
