0e5586fdae59fe6e676ef0b6671aa3347966f143
ivan-rakos <57662193+ivan-rakos@users.noreply.github.com>
2020-05-26 17:51:15 +0200

    Delete PaymentData.py

--- PaymentData.py
+++
@@ -1,9 +0,0 @@
-class PaymentData:
-
-    def __init__(self,tipo,titular,num,cod,imp):
-        self.tipo_pago = tipo
-        self.titular = titular
-        self.num_tarjeta = num
-        self.cod_seguridad = cod
-        self.importe = imp
-        pass
