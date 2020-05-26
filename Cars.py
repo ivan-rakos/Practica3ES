99310838097fa505787790153c8d8aefe0679fbd
ivan-rakos <57662193+ivan-rakos@users.noreply.github.com>
2020-05-26 17:50:53 +0200

    Delete Cars.py

--- Cars.py
+++
@@ -1,7 +0,0 @@
-class Cars:
-
-    def __init__(self, matricula, modelo, asientos):
-        self.matricula = matricula
-        self.modelo = modelo
-        self.asientos = asientos
-        self.disponible = True
